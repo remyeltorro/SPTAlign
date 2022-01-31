b94148563f49395551852093a80937c73df5af05
remy13127 <50580806+remy13127@users.noreply.github.com>
2022-01-31 12:06:22 +0100

    Update sptalign.py

--- scripts/Functions/sptalign.py
+++ scripts/Functions/sptalign.py
@@ -28,8 +28,22 @@ import json
 @njit
 def mean_displacement(x,y,times):
 	"""
-	This function takes a series of positions x and y and computes the average displacement.
-	Function to be refined.
+	Take a single trajectory and compute the average displacement
+	
+	Parameters
+	----------
+	
+	x,y: 1D numpy array
+		the positions x, y as a function of time
+	time: 1D numpy array
+		the time points paired with (X,Y)
+		
+	Returns
+	-------
+	
+	float
+		the average displacement from t to t+1
+	
 	"""
 	fullrange = list(np.linspace(min(times),max(times),int(max(times)-min(times))+1)) #to include gaps in average
 	s=0
@@ -40,9 +54,26 @@ def mean_displacement(x,y,times):
 
 def check_consecutive(frames,dt=1):
 	"""
-	This function looks for consecutive values in the array frames
-	and organises them in chunks of consecutive values [[val1,val1+1,...,val1+n],[val2,val2+1,...]]
+	Look for consecutive values and organise them in chunks 
+	of consecutive values [[val1,val1+1,...,val1+n],[val2,val2+1,...]]
 	with val2!=(val1+n)
+	
+	Parameters
+	----------
+	
+	frames: 1D numpy array
+		the hollow time frames to group in consecutive sequences
+	dt: float
+		time unit calibration 
+		
+	Returns
+	-------
+	
+	numpy array
+		frames organized in consecutive chunks
+	numpy array
+		frames sorted in increasing value
+	
 	"""
 	frames = np.sort(frames)
 	slices = np.zeros(len(frames))
@@ -59,6 +90,28 @@ def check_consecutive(frames,dt=1):
 
 
 def fourier_shift_frame(frame,shift_x,shift_y,calibration):
+	"""
+	Sequentially Fourier transform, apply a shift
+	and Fourier invert an image
+	
+	Parameters
+	----------
+	
+	frame: 2D numpy array
+		the image to shift correct
+	shift_x, shift_y: float
+		the shift in pixel unit to correct in X and Y
+	calibration:
+		pixel to µm calibration (e.g. 1 px = 0.1 µm)
+	
+	Returns
+	-------
+	
+	2D numpy array
+		The shift corrected image
+	
+	"""
+
 	#Fourier transform, apply shift, Fourier invert
 	to_align = np.copy(frame)
 	fft = np.fft.fft2(to_align)
@@ -75,6 +128,28 @@ def fourier_shift_frame(frame,shift_x,shift_y,calibration):
 	return(np.array(np.absolute(fftm1),dtype='uint16'))
 	
 def plot_displacement(frames,x_disp,y_disp,filename):
+	"""
+	Plots the drift along the X and Y axis of the image, 
+	measured from the trajectories
+	
+	Parameters
+	----------
+	
+	frames: 1D numpy array
+		the time frame associated to each displacement value
+	x_disp: 1D numpy array
+		the X-displacement as a function of time in µm
+	y_disp: 1D numpy array
+		the Y-displacement as a function of time in µm
+	filename: str
+		the export filename for the plot
+	
+	Returns
+	-------
+	
+	matplotlib figure
+	
+	"""
 	plt.figure(figsize=(4,3))
 	plt.plot(frames,x_disp,label=r"$x$ displacement")
 	plt.plot(frames,y_disp,label=r"$y$ displacement")
